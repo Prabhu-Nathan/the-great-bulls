de5bc7d79f0fee7b1ed4a8cfa4f9ba6c7e30eec6
prabhu-nathan <prabhunathan24@gmail.com>
2024-12-09 22:59:17 +0530

    Enabled swagger and added dist folder for in ignore changes

--- dist/module/user/mapper.d.ts
+++
@@ -1,6 +0,0 @@
-import { CreateUserDto } from "./dto/createUser.dto";
-import { User } from "./users.schema";
-export declare class Mapper {
-    static toUserResponse(user: User): any;
-    static toUser(createUserDto: CreateUserDto): CreateUserDto;
-}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const requiredEnvVars = [
@@ -18,6 +19,18 @@ async function bootstrap() {
   });
 
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle("Your API Title")
+    .setDescription("Your API description")
+    .setVersion("1.0")
+    .addServer("http://localhost:3000/", "Local environment")
+    .addTag("Your API Tag")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("api-docs", app, document);
+
   await app.listen(process.env.PORT ?? 3000);
   app.useGlobalPipes(
     new ValidationPipe({
