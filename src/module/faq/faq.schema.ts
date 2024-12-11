cdfb04dc2d80131eabb6f3171b78ac0e14869040
Jagadeesh GJ <jagadeesh15508300@gmail.com>
2024-12-11 19:21:03 +0530

    schema corrected

--- src/module/faq/faq.schema.ts
+++ src/module/faq/faq.schema.ts
@@ -1,9 +1,9 @@
 import { Schema, Document } from 'mongoose';
 
 export const FAQSchema = new Schema({
-  question: String,
-  answer: String
-})
+  question: { type: String, required: true },
+  answer: { type: String, required: true },
+}, { timestamps: true })
 
 export interface FAQ extends Document {
   question: string,
