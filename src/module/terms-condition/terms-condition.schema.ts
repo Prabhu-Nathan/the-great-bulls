4bfac993ed683484d02da9629f8620c39e26f642
Jagadeesh GJ <jagadeesh15508300@gmail.com>
2024-12-11 19:41:07 +0530

    TimeStamps added

--- src/module/terms-condition/terms-condition.schema.ts
+++ src/module/terms-condition/terms-condition.schema.ts
@@ -2,7 +2,8 @@ import { Document, Schema } from 'mongoose';
 
 export const TermsConditionSchema = new Schema({
     content: { type: String, required: true }
-})
+}, { timestamps: true })
+
 export interface TermsCondition extends Document {
     content: string
 }
